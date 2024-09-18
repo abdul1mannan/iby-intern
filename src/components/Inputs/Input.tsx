9df82e4ba990e0832f98a22f179562f7d25f169a
Abdul Mannan <mannan.jnv123@gmail.com>
2024-09-18 22:42:44 +0530

    update Input

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -13,7 +13,7 @@ interface FormDefaults extends FieldValues {
 
 interface InputProps {
 	label: string;
-	id: keyof FormDefaults;
+	id: Extract<keyof FormDefaults, string>; // Ensure 'id' is only string keys
 	type?: string;
 	required?: boolean;
 	register: UseFormRegister<FormDefaults>;
@@ -36,7 +36,7 @@ const Input: React.FC<InputProps> = ({ label, id, register, required, errors, ty
 					type={_type}
 					autoComplete={id}
 					disabled={disabled}
-					{...register(id, { required })}  // Register uses keyof FormDefaults now
+					{...register(id, { required })} // Register uses keyof FormDefaults now
 					className={clsx(
 						`form-input form-control block w-full rounded-md border-0 bg-gray-50 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 dark:bg-gray-700 dark:text-gray-200 dark:placeholder-gray-400 dark:ring-gray-500 dark:focus:ring-sky-500 sm:text-sm sm:leading-6`,
 						errors[id] &&
